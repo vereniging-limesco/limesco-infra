5b7a906f42a731a5b39c0b8575aee2867fbaf187
Jille Timmermans <quis@leden.limesco.nl>
2012-03-11 14:32:49 +0100

    Welkom-pagina
    Signed-off-by: Jille Timmermans <quis@leden.limesco.nl>

--- liminfra/leden/views.py
+++ liminfra/leden/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+@login_required
+def welcome(request):
+	return render_to_response('leden/welcome.html', {}, context_instance=RequestContext(request))
