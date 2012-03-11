f61ca2f4d5669849395231cfe76b04de59a0e1d8
Jille Timmermans <quis@limesco.org>
2012-03-11 13:50:40 +0100

    Passwords hernoemd naar sitesettings Inloggen gemaakt
    Signed-off-by: Jille Timmermans <quis@limesco.org>

--- liminfra/urls.py
+++ liminfra/urls.py
@@ -1,17 +1,17 @@
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
+
+from liminfra.settings import MEDIA_ROOT
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'liminfra.views.home', name='home'),
-    # url(r'^liminfra/', include('liminfra.foo.urls')),
+	url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
+	url(r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	url(r'^admin/', include(admin.site.urls)),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+	# Dit kan je beter door je webserver laten doen. Die is daar beter in.
+	(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': MEDIA_ROOT}),
 )
